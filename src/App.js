47fae2c2099eaf0a3f2b1e584986085692249ca0
LucasSilva01 <santoslucas9956@gmail.com>
2021-04-09 23:36:26 -0300

    organization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { Component } from 'react';
+import PostCard from './components/PostCard';
 
 class App extends Component {
   state = {
-    posts: [],
-
+    posts: []
   };
 
   componentDidMount(){
@@ -37,17 +37,10 @@ class App extends Component {
         <div className="posts">
     
           {posts.map(post => (
-            <div  className="post">
-              <img src = {post.cover} alt = {post.title} />
-              <div  className="post-content" key = {post.id}>
-                <h1>
-                  {post.title}
-                  </h1>
-                <p>
-                  {post.body}
-                </p>
-              </div>
-            </div>
+            <PostCard
+              key = {post.id}
+              post = {post}
+            />
             ))}
         </div>
       </section>
